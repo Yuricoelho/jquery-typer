585ea53f9337e9d8ce06991ab90fcc082f1a273f
Yuri <yuricoelho33@gmail.com>
2021-12-05 11:24:37 -0300

    Refatorando o código.

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,54 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-de-digitacao");
 
-campo.on("input", function () {
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-de-palavras").text(qtdPalavras);
+$(
+    function () {
+        atualizaTamanhoFrase();
+        inicializaContadores();
+        inicializaCronometro();
+        $("#botao-reiniciar").click(reiniciaJogo);
+    }
+);
 
-    var qtdCaracteres = conteudo.length;
-    $("#contador-de-caracteres").text(qtdCaracteres);
-});
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function () {
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-de-palavras").text(qtdPalavras);
+    
+        var qtdCaracteres = conteudo.length;
+        $("#contador-de-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function (){
+        var cronometroID = setInterval(function (){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    });
+}
+
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-de-palavras").text("0");
+    $("#contador-de-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
+
