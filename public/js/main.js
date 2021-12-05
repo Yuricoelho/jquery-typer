a83b1bee66a12337ba26a02a9413a62957a968dd
Yuri <yuricoelho33@gmail.com>
2021-12-05 10:26:12 -0300

    Adicionando a contagem de palavras e caracteres no input.

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,14 @@ var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-de-digitacao");
+
+campo.on("input", function () {
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-de-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-de-caracteres").text(qtdCaracteres);
+});
